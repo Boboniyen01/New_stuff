04464ed9778cf9dee1c7eeab26b98e4556169ae5
Abdul-Hakeem Abiola <boboniyenwebdev01@gmail.com>
2024-01-17 21:15:40 +0100

    Final Changes

--- drive.js
+++ drive.js
@@ -1,16 +1,17 @@
 let driveButton = document.getElementById('drive-btn')
 console.log(driveButton)
 let Car = document.getElementById('img-car')
-let headOne = document.querySelector('.head-one')
+let headOne = document.getElementById('first-header')
+var paragraph = document.querySelector('#second-sec p')
 console.dir(headOne)
 function driveTheCar(){
-    if(driveButton){
+    if(Car){
+        headOne.textContent='Congrats!! you have successfully driven Tates car'
         Car.classList.add('cardrive')
+        paragraph.textContent =' Reload Page to Drive the car'
         window.alert("Click 'ok' to start the engine and drive Tate's Car")
-    }else{ console.log('nothing is happening');
-        Car.classList.remove('cardrive')
-    
+    }else if(driveButton){
+        Car.classList.add('cardrive')
     }
-
 }
 driveButton.addEventListener('click',driveTheCar)
